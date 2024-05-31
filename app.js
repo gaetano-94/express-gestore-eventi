ed3a097dff89804be986419bf69f0d846ff1ed51
gaetano-94 <gaetano.panico1994@gmail.com>
2024-05-31 14:20:01 +0200

    feat: milestone 4

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const eventRouter = require('./routers/eventRouter.js');
+const notFoundHandler = require('./middleware/notFoundHandler');
+const errorHandler = require('./middleware/errorHandler');
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api', eventRouter);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server avviato su http://localhost:${port}`);
